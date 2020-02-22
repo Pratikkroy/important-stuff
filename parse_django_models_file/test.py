05464d94950233c2a6d59965c95e7de0bbfe535b
Pratik Roy <pratiekkroy@gmail.com>
2020-02-23 03:59:07 +0530

    not required

--- parse_django_models_file/test.py
+++
@@ -1,9 +0,0 @@
-class Test(models.Model):
-    role_id = models.AutoField(primary_key=True)
-    role_name = models.CharField(max_length=45, blank=True, null=True)
-    parent_role = models.ForeignKey('Roles', models.DO_NOTHING, blank=True, null=True)
-    class Meta:
-        managed = False
-        db_table =  'fitpass_roles'
-
-
