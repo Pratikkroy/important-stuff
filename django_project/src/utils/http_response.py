ed1bce7467a798aa7ff18a358f87e8fa848185a1
Pratik Roy <roypra@amazon.com>
2020-03-10 18:38:32 +0530

    made HttpResponse class instantiated

--- django_project/src/utils/http_response.py
+++ django_project/src/utils/http_response.py
@@ -1,13 +1,22 @@
 import json
-from . import HttpResponseBadFormatException, ClassShouldNotInstantiateException
-
-class HttpResponse:
+from rest_framework.response import Response
+from . import HttpResponseBadFormatException, ClassShouldNotInstantiateException        
+        
+class HttpResponse(Response):
     
-    def __init__(self):
-        raise ClassShouldNotInstantiateException
+    # using "*" to enforce named params
+    def __init__(self, *, http_status, http_message=None, data=None):
+        if type(http_status) is dict:
+            super(HttpResponse, self).__init__(
+                self.__http_response(http_status.get('status'), http_status.get('message'),data)
+            )
+        else:
+            super(HttpResponse, self).__init__(
+                self.__http_response(http_status, http_message, data)
+            )
+             
     
-    @staticmethod
-    def http_response(status_code, message, body=None):
+    def __http_response(self, status_code, message, data=None):
         if status_code is None or message is None:
             print("status_code::{} message::{}".format(status_code,message))
             raise HttpResponseBadFormatException
@@ -15,7 +24,5 @@ class HttpResponse:
         return {
             'statusCode': status_code,
             'message': message,
-            'body': json.dumps(body)
+            'data': json.dumps(data)
         }
-        
-        
