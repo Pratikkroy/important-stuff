a63106ab88673eac86c29ba61bac9fc4f27f894c
Pratik Roy <roypra@amazon.com>
2020-03-10 05:41:11 +0530

    added custom exceptions

---
+++ django_project/src/utils/exceptions.py
@@ -0,0 +1,7 @@
+class HttpResponseBadFormatException(Exception):
+    '''HttpResponse is in bad format'''
+    pass
+
+class ClassShouldNotInstantiateException(Exception):
+    ''' Class should not be instantiated '''
+    pass
