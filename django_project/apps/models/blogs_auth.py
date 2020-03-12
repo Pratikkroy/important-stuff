77a4840436a912c4f681648cf45a1121c6611fd1
Pratik Roy <roypra@amazon.com>
2020-03-13 02:36:35 +0530

    updated script

--- django_project/apps/models/blogs_auth.py
+++ django_project/apps/models/blogs_auth.py
@@ -4,6 +4,9 @@ from . import *
 class BlogsAuth(models.Model):
     auth_id = models.CharField(primary_key=True, max_length=20)
     password = models.CharField(max_length=45)
+    email = models.CharField(unique=True, max_length=45, blank=True, null=True)
+    phone = models.CharField(unique=True, max_length=45, blank=True, null=True)
+    status = models.CharField(max_length=45)
 
     class Meta:
         managed = False
