648da74edaf1458baa216de32c8df129264e7b02
Pratik Roy <roypra@amazon.com>
2020-03-14 04:40:23 +0530

    updated models

--- django_project/apps/models/blogs_auth.py
+++ django_project/apps/models/blogs_auth.py
@@ -2,12 +2,15 @@ from django.db import models
 from . import *
 
 class BlogsAuth(models.Model):
-    auth_id = models.CharField(primary_key=True, max_length=20)
-    password = models.CharField(max_length=45)
+    auth_id = models.CharField(primary_key=True, max_length=50)
+    password = models.CharField(max_length=100)
     email = models.CharField(unique=True, max_length=45, blank=True, null=True)
     phone = models.CharField(unique=True, max_length=45, blank=True, null=True)
     status = models.CharField(max_length=45)
-
+    is_staff = models.IntegerField()
+    last_login = models.DateTimeField()
+    register_time = models.DateTimeField()
+    
     class Meta:
         managed = False
         db_table = 'blogs_auth'
--- django_project/apps/models/blogs_auth_user.py
+++ django_project/apps/models/blogs_auth_user.py
@@ -2,11 +2,13 @@ from django.db import models
 from . import *
 
 class BlogsAuthUser(models.Model):
-    email = models.CharField(primary_key=True, max_length=50)
-    auth = models.ForeignKey(BlogsAuth, models.DO_NOTHING, blank=True, null=True)
-    firstname = models.CharField(max_length=45)
-    lastname = models.CharField(max_length=45)
-
+    username = models.CharField(primary_key=True, max_length=50)
+    auth = models.ForeignKey(BlogsAuth, models.DO_NOTHING)
+    firstname = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=30, blank=True, null=True)
+    group = models.CharField(max_length=45)
+    team = models.CharField(max_length=45)
+    
     class Meta:
         managed = False
         db_table = 'blogs_auth_user'
