67f58d5796ff1c822fc2570e0e74c2723ab46ee3
Pratik Roy <roypra@amazon.com>
2020-03-14 04:38:50 +0530

    validator code updated

--- django_project/middlewares/validator.py
+++ django_project/middlewares/validator.py
@@ -5,7 +5,7 @@ class RequestBodyValidatorMiddleware(MiddlewareMixin):
     def __init__(self, json_schema):
         self.json_schema = json_schema
         
-    def process_request(self, request, *args, **kwargs):
+    def process_request(self, request):
         response = ApiGatewayService.validate_request_body(
             json_schema=self.json_schema, body=request.request.data)
         
