6a5d3750f9f33b37676d5261d9fab26ccc597a94
Pratik Roy <roypra@amazon.com>
2020-03-21 05:37:12 +0530

    code updated

--- django_project/apps/serializers/blogs_auth.py
+++ django_project/apps/serializers/blogs_auth.py
@@ -16,15 +16,26 @@ class BlogsAuthSerializer(serializers.ModelSerializer):
         }
         
     
+        
+    
     def validate(self, data):
         if data['email'] is None and data['phone'] is None:
             raise serializers.ValidationError("Atleast email or phone should be present")
+        
+        # we are not checking if email or phone is unique because
+        # it is already checked checked when the serializer object is created
         return data
     
     # override create method. create_user method hash the password. create method doesn't'
-    # unable to override create_user method so hashong the password
+    # unable to override create_user method so hashing the password
     def create(self, validated_data):
         validated_data['password'] = make_password(validated_data.get('password'))
         return super(BlogsAuthSerializer, self).create(validated_data)
     
+    # Todo: complete below method
+    def update(self, instance, validated_data):
+        pass
     
+    # Todo: complete below method(Optional)
+    # def save(self):
+    #     pass
