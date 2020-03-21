574d054ce5adae2bdd7155cc6fe3a34d41ecdf84
Pratik Roy <roypra@amazon.com>
2020-03-21 05:31:24 +0530

    added utcnow

--- django_project/src/utils/datetime.py
+++ django_project/src/utils/datetime.py
@@ -5,3 +5,7 @@ class DateTime:
     @staticmethod
     def current_datetime():
         return datetime.datetime.now()
+    
+    @staticmethod
+    def current_datetime_utc():
+        return datetime.datetime.utcnow()
